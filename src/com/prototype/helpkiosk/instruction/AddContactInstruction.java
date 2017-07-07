657e09b3e5449cb0fb7c048fa6a062a165474c16
Khine Htwe <khinehtwe@Khines-MacBook-Pro.local>
2017-07-07 16:36:20 -0700

    remove tagcloud and search

--- src/com/prototype/helpkiosk/instruction/AddContactInstruction.java
+++ src/com/prototype/helpkiosk/instruction/AddContactInstruction.java
@@ -1,5 +1,8 @@
 package com.prototype.helpkiosk.instruction;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class AddContactInstruction {
 	
 	private Instruction[] contactInstruction = new Instruction[4];
@@ -11,15 +14,21 @@ public class AddContactInstruction {
 
 
 	private void populateInstruction() {
-		contactInstruction[0] = new Instruction("<html>Tap Contacts <img src=\"img/add-contact-icon.png\"> on the Apps screen</html>", false, new int[]{0}, "addContact", 0, true);
+		ImageIcon img = new ImageIcon("img/cloudtag.png");
+		JLabel tagCloud = new JLabel(img);
+		contactInstruction[0] = new Instruction
+				("<html>Tap Contacts <img src=\"img/add-contact-icon.png\"> on the Apps screen</html>", 
+						false, new int[]{0}, "addContact", 0, true);
 		
-		contactInstruction[1] = new Instruction("<html>Tap <img src=\"img/add-contact-icon.png\"> and select a storage location.</html>"
-												, false, new int[]{0}, "addContact", 1, true);
+		contactInstruction[1] = new Instruction
+				("<html>Tap <img src=\"img/add-contact-icon.png\"> and select a storage location.</html>", 
+						false, new int[]{0}, "addContact", 1, true);
 		
-		contactInstruction[2] = new Instruction("<html>Enter contact information.</html>", true, new int[]{1,11,12,13,2}, "addContact", 2, false);
+		contactInstruction[2] = new Instruction("<html>Enter contact information.</html>", 
+				true, new int[]{1,11,12,13,2}, "addContact", 2, false);
 		
-		contactInstruction[3] = new Instruction("<html>Tap SAVE</html>"
-												, true, new int[]{3,4,5,6}, "addContact", 3, false);
+		contactInstruction[3] = new Instruction("<html>Tap SAVE</html>", 
+				true, new int[]{3,4,5,6}, "addContact", 3, false);
 	}
 	
 	public Instruction[] getInstruction(){
