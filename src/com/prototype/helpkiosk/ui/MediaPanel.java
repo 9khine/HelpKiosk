f6c5c2f65e9f729440c1748300775f8ed64ef8d8
Khine Htwe <khinehtwe@Khines-MacBook-Pro.local>
2017-09-30 21:00:55 -0700

    etc etc

--- src/com/prototype/helpkiosk/ui/MediaPanel.java
+++ src/com/prototype/helpkiosk/ui/MediaPanel.java
@@ -24,9 +24,9 @@ public class MediaPanel extends JPanel {
 	String mediaURLstr;
 	Player mediaPlayer;
 	//EmbeddedMediaPlayer mediaPlayerV = null;
-	
+
 	//private EmbeddedMediaPlayerComponent ourMediaPlayer;
-	
+
 	public MediaPanel() {
 		setLayout(new BorderLayout()); // use a BorderLayout
 	}
@@ -36,13 +36,13 @@ public class MediaPanel extends JPanel {
 		Manager.setHint( Manager.LIGHTWEIGHT_RENDERER, true );
 
 		try {
-			
+
 			if (mediaURL != null) {
-				
+
 				System.out.println("mediaURL is: " + mediaURL);
 				// create a player to play the media specified in the URL
 				mediaPlayer = Manager.createRealizedPlayer(mediaURL);
-				
+
 				System.out.println("mediaPlayer vis component: " + mediaPlayer.getVisualComponent());
 				// get the components for the video and the play-back controls
 				Component video = mediaPlayer.getVisualComponent();
@@ -55,31 +55,65 @@ public class MediaPanel extends JPanel {
 
 				video.addMouseListener(
 						new MouseAdapter() {
+							
+							public void mouseClicked(MouseEvent evt) {
+								mediaPlayer.stop();
+								mediaPlayer.deallocate();
+								
+//								if(evt.getClickCount() == 2 && !evt.isConsumed()) {
+//									Time stop = mediaPlayer.getStopTime();
+//									System.out.println(stop);
+//									mediaPlayer.setMediaTime(stop);
+//									mediaPlayer.start();
+//								}else if(evt.getClickCount() == 1 && !evt.isConsumed()){
+//									mediaPlayer.stop();
+//									Time stop = mediaPlayer.getStopTime();
+//									System.out.println(stop);
+//								}
+								
+//								if(evt.getClickCount() == 2 && !evt.isConsumed()) {
+//									mediaPlayer.stop();
+//									mediaPlayer.deallocate();				
+//								} else if(evt.getClickCount() == 1 && !evt.isConsumed()){
+//									mediaPlayer.prefetch();
+//									mediaPlayer.setMediaTime(new Time(0));
+//									mediaPlayer.start();
+//
+//									Time t = mediaPlayer.getDuration();
+//									int i = 0;
+//									while(mediaPlayer.getStopTime() == t) {
+//										System.out.println(mediaPlayer.getStopTime());
+//										if(i == 4){
+//											break;
+//										}
+//										mediaPlayer.setMediaTime(new Time(0));
+//										mediaPlayer.start();
+//										i++;
+//									}
+//									
+//								} 
+								
+//								else if(evt.getClickCount() == 3 && !evt.isConsumed()){
+//									Time stop = mediaPlayer.getStopTime();
+//									mediaPlayer.setMediaTime(stop);
+//									mediaPlayer.start();
+//								}
+								
+							}
+							public void mousePressed(MouseEvent evt) {
+							}
+							public void mouseReleased(MouseEvent evt) {
+							}	
 							public void mouseEntered(MouseEvent evt) {
 								mediaPlayer.prefetch();
 								mediaPlayer.setMediaTime(new Time(0));
 								mediaPlayer.start();
 							}
 							public void mouseExited(MouseEvent evt) {
-								System.out.println("!!! What is media state - " + mediaPlayer.getState());
-								mediaPlayer.stop();
-							}
-							public void mouseClicked(MouseEvent evt) {
 								mediaPlayer.prefetch();
 								mediaPlayer.setMediaTime(new Time(0));
 								mediaPlayer.start();
-								//mediaPlayer.stop();
 							}
-							public void mousePressed(MouseEvent evt) {
-								//Time pausedTime = mediaPlayer.getMediaTime();
-								//mediaPlayer.stop();
-								//mediaPlayer.setMediaTime(pausedTime);
-								//mediaPlayer.start();
-							}
-
-							public void mouseReleased(MouseEvent evt) {
-								//mediaPlayer.start();
-							}	
 						});	
 
 				if ( video != null ) {
@@ -106,101 +140,117 @@ public class MediaPanel extends JPanel {
 			System.err.println( "Error reading from the source" );
 		} // end catch
 	}
-	
+
 	public String selectURL (String type) {
-		
+
 		String home = System.getProperty("user.home");
 		// TODO: set this to local video folder
 		//String videoFolder = home + "/git/HelpKioskKhine/video";
 		String videoFolder = home + "/git/HelpKiosk/video";		
 		String videoUrl = null;
-		
+
 		// Contact
 		if (type=="openContact") {
-	        videoUrl = "file:/"+videoFolder+"/contacts/contact_open_app.mpeg";
+			videoUrl = "file:/"+videoFolder+"/contacts/contact_open_app.mpeg";
 		} else if(type=="newContact"){
-	        videoUrl = "file://"+videoFolder+"/contacts/contact_add_new.mpeg";
+			videoUrl = "file://"+videoFolder+"/contacts/contact_add_new.mpeg";
 		} else if(type=="enterInfo"){
-	        videoUrl = "file://"+videoFolder+"/contacts/contact_enter_info.mpeg";
+			videoUrl = "file://"+videoFolder+"/contacts/contact_enter_info.mpeg";
 		} else if(type=="saveContact"){
-	        videoUrl = "file://"+videoFolder+"/contacts/contact_save.mpeg";
+			videoUrl = "file://"+videoFolder+"/contacts/contact_save.mpeg";
 		} 
-		
+
 		// Camera
 		else if(type=="openCam"){
-	        videoUrl = "file:"+videoFolder+"/camera/camera_open_app.mpeg";
+			videoUrl = "file:"+videoFolder+"/camera/camera_open_app.mpeg";
 		} else if(type=="takePicture"){
-	        videoUrl = "file:"+videoFolder+"/camera/camera_focus_take.mpeg";
+			videoUrl = "file:"+videoFolder+"/camera/camera_focus_take.mpeg";
 		} 
-		
+
 		// Clock
 		else if(type=="openClock"){
-	        videoUrl = "file:"+videoFolder+"/clock/clock_open_app.mpeg";
+			videoUrl = "file:"+videoFolder+"/clock/clock_open_app.mpeg";
 		} else if(type=="touchAlarm"){
-	        videoUrl = "file:"+videoFolder+"/clock/clock_tap_alarm.mpeg";
+			videoUrl = "file:"+videoFolder+"/clock/clock_tap_alarm.mpeg";
 		} else if(type=="setAlarm"){
-	        videoUrl = "file:"+videoFolder+"/clock/clock_set_time.mpeg";
+			videoUrl = "file:"+videoFolder+"/clock/clock_set_time.mpeg";
 		} else if(type=="saveAlarm"){
-	        videoUrl = "file:"+videoFolder+"/clock/clock_save.mpeg";
+			videoUrl = "file:"+videoFolder+"/clock/clock_save.mpeg";
 		}
-		
+
 		// Message
 		else if(type=="openMessage"){
-	        videoUrl = "file:"+videoFolder+"/message/message_open_app.mpeg";
+			videoUrl = "file:"+videoFolder+"/message/message_open_app.mpeg";
 		} else if(type=="newMessage"){
-	        videoUrl = "file:"+videoFolder+"/message/message_new_tap.mpeg";
+			videoUrl = "file:"+videoFolder+"/message/message_new_tap.mpeg";
 		} else if(type=="enterMessage"){
-	        videoUrl = "file:"+videoFolder+"/message/message_enter_msg.mpeg";
+			videoUrl = "file:"+videoFolder+"/message/message_enter_msg.mpeg";
 		} else if(type=="send"){
-	        videoUrl = "file:"+videoFolder+"/message/message_send.mpeg";
+			videoUrl = "file:"+videoFolder+"/message/message_send.mpeg";
 		}
-		
+
 		// Phone
 		else if(type=="openPhone"){
-	        videoUrl = "file:"+videoFolder+"/phone/phone_open_app.mpeg";
+			videoUrl = "file:"+videoFolder+"/phone/phone_open_app.mpeg";
 		} else if(type=="enterNumber"){
-	        videoUrl = "file:"+videoFolder+"/phone/phone_enter_number.mpeg";
+			videoUrl = "file:"+videoFolder+"/phone/phone_enter_number.mpeg";
 		} else if(type=="call"){
-	        videoUrl = "file:"+videoFolder+"/phone/phone_tap_call.mpeg";
+			videoUrl = "file:"+videoFolder+"/phone/phone_tap_call.mpeg";
 		} 
-		
+
 		// Gallery
 		else if(type=="openGallery"){
-	        videoUrl = "file:"+videoFolder+"/gallery/gallery_open_app.mpeg";
+			videoUrl = "file:"+videoFolder+"/gallery/gallery_open_app.mpeg";
 		} else if(type=="select"){
-	        videoUrl = "file:"+videoFolder+"/gallery/gallery_select.mpeg";
+			videoUrl = "file:"+videoFolder+"/gallery/gallery_select.mpeg";
 		} else if(type=="menuTap"){
-	        videoUrl = "file:"+videoFolder+"/gallery/gallery_menu_tap.mpeg";
+			videoUrl = "file:"+videoFolder+"/gallery/gallery_menu_tap.mpeg";
 		} 
-		
+
 		// None
 		else if(type=="nothing"){
 			videoUrl = "";
 		}
-		
+
 		return videoUrl;
 	}
-	
+
 	public void setURL(URL mediaURL){
 		this.mediaURL = mediaURL;
 	}
+
+	//	for(int i = 0; i <= 3; i++) {
+	//	System.out.print("Playing... " + i);
+	//	new Thread(
+	//			new Runnable() {
+	//				public void run() {
+	//					try {
+	//						mediaPlayer.prefetch();
+	//						mediaPlayer.setMediaTime(new Time(0));
+	//						mediaPlayer.start();
+	//					} catch (Exception e) {
+	//						e.printStackTrace();
+	//					}
+	//				}
+	//			}).start();
+	//}
 	
-//	//	https://stackoverflow.com/questions/20440484/embed-a-youtube-video-to-jframe
-//	// 	http://djproject.sourceforge.net/ns/
-//	public JPanel getBrowserPanel() {
-//	    JPanel webBrowserPanel = new JPanel(new BorderLayout());
-//	    JWebBrowser webBrowser = new JWebBrowser();
-//	    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
-//	    webBrowser.setBarsVisible(false);
-//	    webBrowser.navigate("https://www.youtube.com/watch?v=3tmd-ClpJxA");
-//	    return webBrowserPanel;
-//	}
-	
+	//	//	https://stackoverflow.com/questions/20440484/embed-a-youtube-video-to-jframe
+	//	// 	http://djproject.sourceforge.net/ns/
+	//	public JPanel getBrowserPanel() {
+	//	    JPanel webBrowserPanel = new JPanel(new BorderLayout());
+	//	    JWebBrowser webBrowser = new JWebBrowser();
+	//	    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+	//	    webBrowser.setBarsVisible(false);
+	//	    webBrowser.navigate("https://www.youtube.com/watch?v=3tmd-ClpJxA");
+	//	    return webBrowserPanel;
+	//	}
+
 
 	//	http://lethalman.blogspot.ca/2009/02/jmf-and-mpeg.html
 	//	http://jffmpeg.sourceforge.net/
-//	Format[] inFormats = { new VideoFormat ("MPEG") };
-//	PlugInManager.addPlugIn ("net.sourceforge.jffmpeg.VideoDecoder", inFormats, null, PlugInManager.CODEC);
-//	PlugInManager.commit ();
-	
+	//	Format[] inFormats = { new VideoFormat ("MPEG") };
+	//	PlugInManager.addPlugIn ("net.sourceforge.jffmpeg.VideoDecoder", inFormats, null, PlugInManager.CODEC);
+	//	PlugInManager.commit ();
+
 } // end class MediaPanel
